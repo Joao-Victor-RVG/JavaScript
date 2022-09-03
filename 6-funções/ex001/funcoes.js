fb6c0b1f5257cdc69d38e31a2970bcf84b758caa
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2022-09-03 16:28:58 -0300

    funções

---
+++ 6-funções/ex001/funcoes.js
@@ -0,0 +1,19 @@
+
+// Declaration - Declaracão da funcão
+//Function statement
+function creatPhrases( ) {
+    console.log(`Ola mundo`)
+    console.log(`Tudo bem?`)
+    console.log(`Tudo certo?`)
+}
+
+// Executar a funcão
+//Execute, run, call, invoke
+
+creatPhrases( )
+
+console.log(`fim do programa`)
+
+
+
+
