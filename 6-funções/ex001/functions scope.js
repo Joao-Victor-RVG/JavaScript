1a07a31adeeef5d2edc1a0ca873ce615de97bc9b
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2022-09-14 17:00:36 -0300

    Functions Scope

--- 6-funções/ex001/functions scope.js
+++ 6-funções/ex001/functions scope.js
@@ -2,8 +2,13 @@
 
 let subject
 function createThink(subject) {
+    subject = 'study'
     return subject
 }
 
 
+
+console.log(subject)
+console.log(createThink())          // or creatThing()
+console.log(subject)
 
